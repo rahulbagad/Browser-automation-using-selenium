d324a4952abd6a74c63610c6f351c1f128c6dd45
Rahul Bagad <rahul.bagad@walchandsangli.ac.in>
2017-11-02 15:04:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome('C:\Users\IT Lab\Desktop\driver\chromedriver')
-driver.get('http://puzzlepedia.esy.es/project/login.php');
+driver = webdriver.Chrome('path-to-chromedriver')
+driver.get('http://example.com/');
 
 username = driver.find_element_by_id("email")
-username.send_keys("rlbagad2@gmail.com")
+username.send_keys("email")
+username = driver.find_element_by_id("password")
+username.send_keys("pass@123")
 driver.find_element_by_name("data").click()
 
 time.sleep(5) 
