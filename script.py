d356d2d91af3940a2e6668ac2f6fcde03261bca2
Rahul Bagad <rahul.bagad@walchandsangli.ac.in>
2017-11-02 15:15:00 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,15 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome('path-to-chromedriver')
-driver.get('http://example.com/');
+driver.get('https://github.com/login');
 
-username = driver.find_element_by_id("email")
-username.send_keys("email")
+username = driver.find_element_by_id("login_field")
+username.send_keys("username")
+
 username = driver.find_element_by_id("password")
-username.send_keys("pass@123")
-driver.find_element_by_name("data").click()
+username.send_keys("pass123")
+
+driver.find_element_by_name("commit").click()
 
 time.sleep(5) 
 driver.quit()
